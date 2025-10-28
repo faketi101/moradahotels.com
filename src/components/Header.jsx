f5e9bf75d5196a66cab0436d147efdc8913217bd
Tarikul Islam <tarikul@tarikul.dev>
2025-10-28 11:26:10 +0600

    scrollable header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { Menu } from "lucide-react";
 
-const navigation =[
-    {
-        name:"Our Rooms",
-        href:"/our-rooms"
-    },
-    {
-        name:"Restraunt",
-        href:"/restaraunt"
-    },
-    {
-        name:"Meetings & Events",
-        href:"/meetings-events"
-    },
-    {
-        name:"Book Now",
-        href:"/book-now"
-    },
-    {
-        name:"Contact",
-        href:"/contact"
-    }
-]
-
-
+const navigation = [
+  {
+    name: "Our Rooms",
+    href: "/our-rooms",
+  },
+  {
+    name: "Restraunt",
+    href: "/restaraunt",
+  },
+  {
+    name: "Meetings & Events",
+    href: "/meetings-events",
+  },
+  {
+    name: "Book Now",
+    href: "/book-now",
+  },
+  {
+    name: "Contact",
+    href: "/contact",
+  },
+];
 
 const Header = ({ isHero }) => {
   const [scrolled, setScrolled] = useState(false);
@@ -37,15 +35,18 @@ const Header = ({ isHero }) => {
       }
     };
 
-    window.addEventListener('scroll', handleScroll, { passive: true });
+    window.addEventListener("scroll", handleScroll, { passive: true });
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, [scrolled]);
 
   return (
-    <header 
-      className={`${isHero && !scrolled ? "bg-transparent" : "bg-black"} py-4 fixed top-0 left-0 right-0 z-50 transition-colors duration-300`}>
+    <header
+      className={`${
+        isHero && !scrolled ? "bg-transparent" : "bg-black"
+      } py-4 fixed top-0 left-0 right-0 z-50 transition-colors duration-300`}
+    >
       <div className="mx-auto max-w-7xl">
         <div className="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
           <div className="flex-shrink-0">
@@ -60,7 +61,9 @@ const Header = ({ isHero }) => {
               <a
                 key={item.name}
                 href={item.href}
-                className={`inline-flex items-center px-1 pt-1 text-sm ${isHero ? "text-white" : "text-white"} font-lato`}
+                className={`inline-flex items-center px-1 pt-1 text-sm ${
+                  isHero ? "text-white" : "text-white"
+                } font-lato`}
               >
                 {item.name}
               </a>
